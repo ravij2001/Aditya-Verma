22917ce3c27124f43724656567feeb97a3906095
Ravij Parikh <Ravijparikh0@gmail.com>
2024-11-20 14:55:27 +0530

    nearest greater to right program

---
+++ Stack/nearest_greater_to_right.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[5] = {1, 4, 2, 3, 5};
+    int n = 5;
+    vector<int> v; 
+    stack<int> s;
+
+    for(int i = n - 1 ; i >= 0 ; i--){
+        if(s.size() == 0){
+             v.push_back(-1);
+        }
+        else if(s.size() > 0 && s.top() > arr[i]){
+            v.push_back(s.top());
+        }
+        else if(s.size() > 0 && s.top() <= arr[i]){
+            while(s.size() > 0 && s.top() <= arr[i]){
+                s.pop();
+            }
+            if(s.size() == 0){
+                v.push_back(-1);
+            }
+            else{
+                v.push_back(s.top());
+            }
+        }
+            s.push(arr[i]);
+    }
+    reverse(v.begin(), v.end());
+    for (auto i : v)
+        cout << i << " ";
+    return 0;
+}
