7f4904f7130715378b19f7753244b00b7f2c2401
Ravij Parikh <Ravijparikh0@gmail.com>
2024-12-08 12:08:04 +0530

    K largest elements Program

--- Heap/k_largest_elements.cpp
+++ Heap/k_largest_elements.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void KLargestElement(int arr[5],int k,int n){
-    priority_queue <int> minHeap;
+     priority_queue <int, vector<int>, greater<int>> minHeap; 
     for(int i = 0 ; i < n; i++){
         minHeap.push(arr[i]);
         if(minHeap.size() > k){
@@ -10,17 +10,17 @@ void KLargestElement(int arr[5],int k,int n){
         }
     }
     while(minHeap.size() > 0){
-        cout<<minHeap.top()<<endl;
+        cout<<minHeap.top()<<" "<<endl;
         minHeap.pop();
-    }
+    } 
 }
 
 
 int main(){
-    int k,n = 5;
-    int arr[5] = {1,2,5,4,3};
+    int k,n = 10;
+    int arr[10] = {1,2,5,4,3,9,8,6,7,10};
     cin>>k;
-    cout<<"value of k is :"<<k<<endl;
-     KLargestElement(arr,k,n);
+    cout<<"value of k is :"<<k<<endl;    
+ KLargestElement(arr,k,n);
     return 0;
 }  
