e5fb3644837417c3e42d89b8f56061c774623886
Ravij Parikh <Ravijparikh0@gmail.com>
2025-07-21 19:46:04 +0530

    anagram strings

--- String/Anagrams.cpp
+++ String/Anagrams.cpp
@@ -1,6 +1,9 @@
 #include <algorithm>
 #include <iostream>
+#include<unordered_map>
 using namespace std;
+
+// brute force approach with time complexity o(nlogn) and space complexity 0(1)
 
 bool areAnagrams(string &s1, string &s2) {
     
@@ -14,6 +17,32 @@ bool areAnagrams(string &s1, string &s2) {
     // Compare sorted strings
     return (s1 == s2);
 }
+
+
+
+class Solution2 {
+public:
+    bool isAnagram(string s, string t) {
+          if (s.length() != t.length()) return false;
+
+        unordered_map<char, int> count;
+
+        for (char c : s) {
+            count[c]++;
+        }
+
+        for (char c : t) {
+            count[c]--;
+        }
+        for (auto& pair : count) {
+            if (pair.second != 0) return false;
+        }
+
+        return true;
+    }
+};
+
+
 
 int main() {
     
